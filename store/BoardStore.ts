71d99b50f1ce25ecb11b5bc3595a0eff05a348df
Wertnother <hakageblazer@gmail.com>
2023-07-19 20:18:35 +0300

    Add Modal window

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { databases } from '@/appwrite';
+import { databases, storage} from '@/appwrite';
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
 import { Board, Column, Todo, TypedColumn } from '@/typings';
 import { create } from 'zustand'
@@ -8,12 +8,30 @@ interface BoardState {
     getBoard: () => void;
     setBoardState: (board: Board) => void;
     updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
+    newTaskInput: string;
+    newTaskType: TypedColumn;
+    image: File | null;
+
+    searchString: string;
+    setSearchSring: (searchString: string) => void;
+
+    deleteTask: (taskIndex: number, todoId: Todo, id: TypedColumn) => void;
+    
+    setNewTaskInput: (input: string) => void;
+    setNewTaskType: (columnId: TypedColumn) => void;
+    setImage: (image: File | null) => void;
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: {
     columns: new Map<TypedColumn, Column>(),
   },
+  searchString: '',
+  newTaskInput: '',
+  newTaskType: 'todo',
+  image: null,
+
+  setSearchSring: (searchString) => set({searchString}),
   
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
@@ -21,6 +39,26 @@ export const useBoardStore = create<BoardState>((set) => ({
   },
 
   setBoardState: (board) => set({board}),
+
+  deleteTask: async (taskIndex: number, todo: Todo, id: TypedColumn) => {
+    const newColumns = new Map(get().board.columns);
+
+    set({board: {columns: newColumns}});
+    
+    if(todo.image) {
+      await storage.deleteFile(todo.image.bucketId, todo.image.fileId);
+    };
+
+    await databases.deleteDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLECTION_ID!,
+      todo.$id,
+    )
+  },
+
+  setNewTaskInput: (input: string) => set({newTaskInput: input}),
+  setNewTaskType: (columnId: TypedColumn) => set({newTaskType: columnId}),
+  setImage: (image: File | null) => set({image}),
 
   updateTodoInDB: async(todo, columnId) => {
     await databases.updateDocument(
